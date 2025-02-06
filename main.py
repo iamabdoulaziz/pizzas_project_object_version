91500755efa21e5c98d06165b1bb99eebe4a7080
Cute Boy <abdoulazizc867@gmail.com>
2025-02-06 01:40:51 +0000

    # I add new parameters like ingredients, vegetarien, and modified the print method.

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
-# Let's start the project
+# I add new parameters like ingredients, vegetarien, and modified the print method
+# On comments it is my own code before checking the solution
 
 
 class Pizza:
-    def __init__(self, name, price):
+    def __init__(self, name, price, ingredients, vegetarian = False): # vegetarian = ""
         self.name = name
         self.price = price
+        self.ingredients = ingredients
+        self.vegetarien = vegetarian
+
 
 
     def print(self):
-        print(f"PIZZA {self.name} : {self.price}")
+        veg_str = ""
+        if self.vegetarien:
+            veg_str = "- VEGETARIAN"
+            #print(f"PIZZA {self.name} : {self.price} euros - VEGETARIAN")
+        # else:
+        #     print(f"PIZZA {self.name} : {self.price} euros")
+        print(f"PIZZA {self.name} : {self.price} euros {veg_str}")
+        print(", ".join(self.ingredients))
+        print()
 
 
-pizza = Pizza("4 fromages", 8.5)
-pizza.print()
+# Pizza("4 fromages", 8.5, ("bris", "emmental", "compté", "parnesan"))
+# pizza.print()
+
+pizzas = (
+    Pizza("4 fromages", 8.5, ("bris", "emmental", "compté", "parnesan"), True),
+    Pizza("Cutence Pizza", 10.5, ("farine", "fromage", "poulet", "tomates")),
+    Pizza("Cute_girl P", 12.5, ("Cho", "banane plantin", "poulet", "huile d'olive")),
+    Pizza("Végétarienne", 7.8, ("Champignons", "tomate", "oignons", "poivrons"), True),
+
+)
+
+for pizz in pizzas:
+    pizz.print()
+
